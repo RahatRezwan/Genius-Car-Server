0752c5d675835b5425dec1c6cb9d2d1aa7d24e67
Kazi Rahat <kazirahat1007@outlook.com>
2022-11-02 09:36:17 +0600

    create genius car server and connect with mongodb

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+   res.send("Genius car server is running");
+});
+
+/* mongodb setup and connect */
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@cluster0.wqgorgk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
+});
+
+app.listen(port, () => {
+   console.log("server is running on port:", port);
+});
