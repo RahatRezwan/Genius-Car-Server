c7aee20f76f9cb9ba84a50848e97fc947effdbca
Kazi Rahat <kazirahat1007@outlook.com>
2022-11-02 14:56:07 +0600

    create services api and orders api and display them

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -19,11 +19,50 @@ const client = new MongoClient(uri, {
    useUnifiedTopology: true,
    serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-   const collection = client.db("test").collection("devices");
-   // perform actions on the collection object
-   client.close();
-});
+
+const run = async () => {
+   try {
+      /* services collection */
+      const serviceCollection = client.db("geniusCar").collection("services");
+
+      /* Orders Collection */
+      const ordersCollection = client.db("geniusCar").collection("orders");
+
+      app.get("/services", async (req, res) => {
+         const query = {};
+         const cursor = serviceCollection.find(query);
+         const services = await cursor.toArray();
+         res.send(services);
+      });
+
+      app.get("/service/:id", async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: ObjectId(id) };
+         const service = await serviceCollection.findOne(query);
+         res.send(service);
+      });
+
+      /* Orders API */
+      app.post("/orders", async (req, res) => {
+         const order = req.body;
+         const result = await ordersCollection.insertOne(order);
+         res.send(result);
+      });
+
+      /* get orders by query */
+      app.get("/orders", async (req, res) => {
+         let query = {};
+         if (req.query.email) {
+            query = { email: req.query.email };
+         }
+         const cursor = ordersCollection.find(query);
+         const orders = await cursor.toArray();
+         res.send(orders);
+      });
+   } finally {
+   }
+};
+run().catch((e) => console.log(e));
 
 app.listen(port, () => {
    console.log("server is running on port:", port);
